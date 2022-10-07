9dceb5468e8751428038f3e870efcbea96ce4980
dvgba <72717322+dvgba@users.noreply.github.com>
2022-10-07 10:40:46 -0300

    03

---
+++ Diego Viana/1008/1008.py
@@ -0,0 +1,8 @@
+ID = int(input())
+HR = int(input())
+VH = float(input())
+
+RE = float(HR * VH)
+
+print("NUMBER = %d" %ID)
+print("SALARY = U$ %0.2f" %RE)
---
+++ Diego Viana/1009/1009.py
@@ -0,0 +1,9 @@
+ID = input()
+RE = float(input())
+QTD = float(input())
+
+BO = float(QTD * (15/100))
+
+total = RE + BO
+
+print("TOTAL = R$ %0.2f" %total)
